f456bb6f61f1ed1cb56db51e3a398d7351151d88
AxelAzriel27 <4xelkopalit@gmail.com>
2025-02-10 09:24:50 +0800

    pertemuan 6

--- index.js
+++ index.js
@@ -1,32 +1,47 @@
 const http = require("http");
+const { hello, greetings } = require("./helloWorld");
 const moment = require("moment");
-const users = require("./users");
+
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/json");
+  // res.write(hello)
+  // res.write(greetings())
   const url = req.url;
-  if (url === "/") {
-    res.write("This is the home page");
-  } else if (url === "/about") {
+  if (url === "/users") {
     res.statusCode = 200;
+    res.setHeader("Content-Type", "text/json");
     res.write(
       JSON.stringify({
         status: "success",
-        message: "respone success",
-        description: "Exercise #02",
-        date: moment().format(),
+        message: "Users",
+        date: moment().format("MMMM Do YYYY, h:mm:ss a"),
       })
     );
-  } else if (url === "/users") {
+  } else if (url === "/post") {
+    res.statusCode = 200;
     res.setHeader("Content-Type", "text/json");
-    res.write(JSON.stringify(users));
+    res.write(
+      JSON.stringify({
+        status: "success",
+        message: "Post",
+        date: moment().format("MMMM Do YYYY, h:mm:ss a"),
+      })
+    );
+  } else {
+    res.statusCode = 404;
+    res.setHeader("Content-Type", "text/json");
+    res.write(
+      JSON.stringify({
+        status: "not found",
+        message: "Route tidak ditemukan",
+        date: moment().format("MMMM Do YYYY, h:mm:ss a"),
+      })
+    );
   }
-
   res.end();
 });
 
 const hostname = "127.0.0.1";
 const port = 3000;
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+server.listen(port, hostname, () =>
+  console.log(`Server running at http://${hostname}:${port}`)
+);
