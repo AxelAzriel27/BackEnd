ba3d19b0878bd32a1d4045e39c9ee8dc4288f2b3
AxelAzriel27 <4xelkopalit@gmail.com>
2025-02-12 09:21:41 +0800

    Latihan 3

--- index.js
+++ index.js
@@ -1,48 +1,29 @@
-const http = require("http");
 const express = require("express");
+const moment = require("moment");
+const users = require("./users");
+
 const app = express();
-
-app.get("/", (req, res) => res.send("Hello World!"));
-
-app.get("/contoh", (req, res) => {
-  res.send("request dengan method GET");
-});
-app.get("/about", (req, res) =>
-  res.status(200).json({
-    status: "success",
-    message: "About page",
-    data: [],
-  })
-);
-
-app.post("/contoh", (req, res) => {
-  res.send("request dengan method POST");
-});
-
-app.put("/contoh", (req, res) => {
-  res.send("request dengan method PUT");
-});
-
-app.delete("/contoh", (req, res) => {
-  res.send("request dengan method DELETE");
-});
-
-app.patch("/contoh", (req, res) => {
-  res.send("request dengan method PATCH");
-});
-
-app.all("/universal", (req, res) => res.send(`Request method ${req.method}`));
-// Routing dinamis
-// 1. Menggunakan params
-app.get("/post/:id", (req, res) => res.send(`Artikel ke - ${req.params.id}`));
-// 2. Menggunakan Query String
-app.get("/post", (req, res) => {
-  const { page, sort } = req.query;
-  res.send(`Query yang didapatkan adalah : ${page}, sort : ${sort}`);
-});
-
-const hostname = "127.0.0.1";
 const port = 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("This is the home page");
+});
+
+app.get("/about", (req, res) => {
+  res.json({
+    status: "success",
+    message: "response success",
+    description: "Exercise #02",
+    date: moment().format(),
+  });
+});
+
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+
 app.listen(port, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://127.0.0.1:${port}/`);
 });
