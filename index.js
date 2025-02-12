d8f4696fd6d316b79da858cc88f406126c001520
Axel Kopalit <145676270+AxelAzriel27@users.noreply.github.com>
2025-02-12 09:30:44 +0800

    Fixing Latihan3

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ const users = require("./users");
 const app = express();
 const port = 3000;
 
-app.use(express.json());
-
 app.get("/", (req, res) => {
   res.status(200).send("This is the home page");
 });
