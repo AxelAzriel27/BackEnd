525f68851ed21000bd8562f8329e9f3e460f893c
AxelAzriel27 <4xelkopalit@gmail.com>
2025-02-19 09:42:36 +0800

    Latihan 4

--- index.js
+++ index.js
@@ -1,26 +1,42 @@
-const express = require("express");
+const http = require("http");
 const moment = require("moment");
+const express = require("express");
+const morgan = require("morgan");
+const errorhandler = require("errorhandler");
 const users = require("./users");
-
 const app = express();
-const port = 3000;
 
-app.get("/", (req, res) => {
-  res.status(200).send("This is the home page");
-});
-app.get("/about", (req, res) =>
-  res.status(200).json({
-    status: "success",
-    message: "respone success",
-    description: "Exercise #03",
-    date: moment().format(),
-  })
-);
+app.use(morgan("tiny"));
 
 app.get("/users", (req, res) => {
-  res.status(200).json(users);
+  res.json(users);
 });
 
-app.listen(port, () => {
-  console.log(`Server running at http://127.0.0.1:${port}/`);
+app.get("/users/:name", (req, res) => {
+  const name = req.params.name.toLowerCase();
+  const user = users.find((u) => u.name.toLowerCase() === name);
+
+  if (!user) {
+    return res.status(404).json({ message: "Data user tidak ditemukan" });
+  }
+  res.json(user);
 });
+
+app.use((req, res, next) => {
+  res
+    .status(404)
+    .json({ status: "error", message: "resource tidak ditemukan" });
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res
+    .status(500)
+    .json({ status: "error", message: "terjadi kesalahan pada server" });
+});
+
+const hostname = "127.0.0.1";
+const port = 3000;
+app.listen(port, hostname, () =>
+  console.log(`Server running at http://${hostname}:${port}`)
+);
